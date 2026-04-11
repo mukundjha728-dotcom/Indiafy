7d3f66066c630a354e70d085c273c3770a60fce8
Roshan0333 <ra786roshanansari@gmail.com>
2026-04-11 13:00:59 +0530

    seller model

---
+++ backend/models/sellers/auth.model.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+import {passwordEncryption} from "../../utils/bcrypt.js"
+
+const authSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    middleName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},
+    { timestamps: true }
+)
+
+authSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await passwordEncryption(this.password);
+
+    return next();
+})
+
+const authModel = mongoose.model("seller", authSchema);
+
+export default authModel;
